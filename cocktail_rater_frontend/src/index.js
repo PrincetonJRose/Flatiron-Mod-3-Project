7c02c4cf69e51a0585d77e9c81b3378077d58665
PrincetonJRose <decoyzpr@gmail.com>
2019-05-07 10:39:38 -0400

    Added search bar to left grid pane for site along with a header with the site name.

--- cocktail_rater_frontend/src/index.js
+++ cocktail_rater_frontend/src/index.js
@@ -1,5 +1,13 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const search = document.getElementById('site-search')
+search.addEventListener('submit', siteSearch)
 
 const cocktailsUrl = `http://localhost:3000/cocktails/`
 const ingredientsUrl = `http://localhost:3000/ingredients/`
+
+function siteSearch(e) {
+    console.log("Hello")
+    e.preventDefault()
+    e.target.reset()
+}
