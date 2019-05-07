2160d39017a70c1b350eb86c55eea5c6f9f82480
PrincetonJRose <decoyzpr@gmail.com>
2019-05-07 08:13:22 -0400

    Changed folder for cocktail rater to backend to seperate front and back. Also mostly finished algorithm to pull all the data from the API.

---
+++ cocktail_rater_backend/app/api_mass_pull.rb
@@ -0,0 +1,45 @@
+ingredient_response = JSON.parse(RestClient.get("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"))
+
+ingredients_names = []
+for index in 0...ingredient_response['drinks'].size
+    ingredients_names << ingredient_response['drinks'][index]['strIngredient1']
+end
+
+ingredients_info = []
+i = 0
+ingredients_names.each do |name|
+    if i == 22 or i == 115
+        ingredient_info = JSON.parse(RestClient.get("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=#{i}"))
+        ingredients_info << ingredient_info
+        i += 1
+        next
+    end
+    ingredient_info = JSON.parse(RestClient.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?i=#{name}"))
+    ingredients_info << ingredient_info
+    #save to db here
+    i += 1
+end
+
+drink_ids = []
+i = 0
+ingredients_names.each do |name|
+    if i == 22 or i == 115
+        i += 1
+        next
+    end
+    ingredients_drinks = JSON.parse(RestClient.get("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=#{name}"))
+    ingredients_drinks['drinks'].each do |drink|
+        drink_ids << drink['idDrink']
+    end
+    i += 1
+end
+
+drink_ids = drink_ids.uniq
+
+cocktails = []
+drinks.each do |id|
+    drink_info = JSON.parse(RestClient.get("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=#{id}"))
+    cocktails << drink_info
+    # save drink info into db
+end
+
