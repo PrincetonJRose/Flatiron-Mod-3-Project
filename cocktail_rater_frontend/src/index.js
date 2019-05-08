165494a471b9f35b6c5a0093d9cb9b3bec433c2d
PrincetonJRose <decoyzpr@gmail.com>
2019-05-08 11:15:55 -0400

    Added in ability for right div to be scrollable, added in login/logout to left nav pane.

--- cocktail_rater_frontend/src/index.js
+++ cocktail_rater_frontend/src/index.js
@@ -1,12 +1,21 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+
 const search = document.getElementById('site-search')
 search.addEventListener('submit', siteSearch)
+
 const navList = document.getElementById('nav-list')
 const mainDisplay = document.getElementById('main-display')
+mainDisplay.style.overflowX = 'hidden'
+mainDisplay.style.overflowY = 'auto'
+mainDisplay.style.height = '600px'
+mainDisplay.style.float = 'right'
+
+let user = []
 
 const cocktailsUrl = `http://localhost:3000/cocktails/`
 const ingredientsUrl = `http://localhost:3000/ingredients/`
+const usersUrl = `http://localhost:3000/users/`
 
 function siteSearch(e) {
     e.preventDefault()
@@ -19,23 +28,18 @@ function clearNode(node) {
     }
 }
 
-function makeNavList() {
-    clearNode(navList)
-    navItems = ['Home', 'Cocktails', 'Ingredients', 'Custom Drinks', 'Favorites', 'About']
-    for ( let item of navItems ) {
-        let br = document.createElement('br')
-        let li = document.createElement('li')
-        li.innerHTML = item
-        li.addEventListener('click', (e) => {
-            navMenu(e, item)
-        })
-        navList.appendChild(br)
-        navList.appendChild(li)
-    }
-}
-
 function homePage() {
     clearNode(mainDisplay)
+
+    if (user.length != 0) {
+        let h3 = document.createElement('h3')
+        h3.className = `center-element`
+        h3.innerHTML = `Hello ${user[0]}!`
+        h3.setAttribute('align', 'center')
+        let br = document.createElement('br')
+        mainDisplay.appendChild(h3)
+        mainDisplay.appendChild(br)
+    }
 
     let h1 = document.createElement('h1')
     h1.innerHTML = `Welcome to Cocktail Rater`
@@ -54,7 +58,28 @@ function homePage() {
     }
     img.className = "center-img"
     mainDisplay.appendChild(img)
+}
 
+function makeNavList() {
+    clearNode(navList)
+    navItems = ['Home', 'Cocktails', 'Ingredients', 'Custom Drinks', 'Favorites', 'About', 'loginStatus']
+    for ( let item of navItems ) {
+
+        if (item == 'loginStatus' && user.length == 0) {
+            item = 'Login'
+        } else if (item == 'loginStatus' && user.length != 0) {
+            item = 'Logout'
+        }
+
+        let br = document.createElement('br')
+        let li = document.createElement('li')
+        li.innerHTML = item
+        li.addEventListener('click', (e) => {
+            navMenu(e, item)
+        })
+        navList.appendChild(br)
+        navList.appendChild(li)
+    }
 }
 
 function navMenu(e, menu) {
@@ -76,7 +101,14 @@ function navMenu(e, menu) {
     if (menu == 'Favorites') {
         getFavorites()
     }
+    if (menu == 'Login') {
+        loginUser()
+    }
+    if (menu == 'Logout') {
+        logoutUser()
+    }
 }
+
 
 makeNavList()
 homePage()
