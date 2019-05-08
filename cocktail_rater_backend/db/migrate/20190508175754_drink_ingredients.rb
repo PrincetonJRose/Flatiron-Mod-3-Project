516355352efc60d7237b054acf698bd8b78856df
PrincetonJRose <decoyzpr@gmail.com>
2019-05-08 17:22:12 -0400

    Added in drink ingredients table columns to migration files.

--- cocktail_rater_backend/db/migrate/20190508175754_drink_ingredients.rb
+++ cocktail_rater_backend/db/migrate/20190508175754_drink_ingredients.rb
@@ -1,4 +1,11 @@
 class DrinkIngredients < ActiveRecord::Migration[5.2]
   def change
+    create_table :drink_ingredients do |t|
+      t.integer :drink_id
+      t.integer :ingredient_id
+      t.text :measurement
+
+      t.timestamps
+    end
   end
 end
